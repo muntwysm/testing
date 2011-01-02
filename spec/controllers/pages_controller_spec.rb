3d4198221ba166b447e75839bff3ac8dbe2f0fba
muntwysm <smuntwyler@zis.ch>
2011-01-02 15:16:01 +0100

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,16 +3,21 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    # Define @base_title here.
+    @base_title = "MyTwitt App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
     
-    it "should have the right title" do
+    it "should have the right title ******" do
       get 'home'
       response.should have_tag("title",
-                               "MyTwitt | Home")
+                               @base_title + " | Home")
     end
   end
 
@@ -25,7 +30,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_tag("title",
-                               "MyTwitt | Contact")
+                               @base_title + " | Contact")
     end
   end
 
@@ -38,7 +43,20 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_tag("title",
-                               "MyTwitt | About")
+                               @base_title + " | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title",
+                               @base_title + " | Help")
     end
   end
 end
