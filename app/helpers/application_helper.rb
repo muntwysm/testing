4b49192c215ad50b48f4e73272a7b9992e469419
muntwysm <smuntwyler@zis.ch>
2011-01-02 17:15:24 +0100

    Finished first cut of the USer Model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  def logo
+    @logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+    return @logo
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "MyTwitt App"
